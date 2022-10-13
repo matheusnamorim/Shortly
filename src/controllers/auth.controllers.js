ea6cd9204a3befb59353fa86c698320e04d95101
Amorim <matheusnavesamorim@gmail.com>
2022-10-13 19:34:57 -0300

    Feat: registro de usuarios

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,11 +1,17 @@
 import { connection } from "../db/db.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
 import { MESSAGES } from "../enums/messages.js";
+import bcrypt from 'bcrypt';
 
 const registerUser = (req, res) => {
-    const { user } = res.locals;
+    const { name, email, password } = res.locals.user;
     try {
-        return res.send('ok');
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        connection.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, 
+        [name, email, passwordHash]);
+
+        return res.sendStatus(STATUS_CODE.OK);
     } catch (error) {
         return res.status(STATUS_CODE.SERVER_ERROR).send(MESSAGES.SERVER_ERROR);
     }
